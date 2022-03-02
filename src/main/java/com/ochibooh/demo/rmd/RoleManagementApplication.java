631712be3f4b9fe0b65ca026f7554727b5ed92af
Phelix Ochieng <9phelix1@gmail.com>
2022-03-02 22:09:21 +0300

    Init

--- src/main/java/com/ochibooh/demo/rmd/RoleManagementApplication.java
+++ src/main/java/com/ochibooh/demo/rmd/RoleManagementApplication.java
@@ -2,7 +2,9 @@ package com.ochibooh.demo.rmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
 @SpringBootApplication
 public class RoleManagementApplication {
     public static void main(String[] args) {
